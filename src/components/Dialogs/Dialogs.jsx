ed75ed00c4a0af9a7a82ff1379c3310feee917c9
Maxim Shtin <maksimshtin@gmail.com>
2025-07-09 17:39:10 +0500

    Uses class component for users, first try of useSelector and useDispatch

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,33 @@
 import classes from "./Dialogs.module.css"
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import React from "react";
+import React, {useCallback} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {addMessageActionCreator, messageTextChangeActionCreator} from "../../redux/dialogsReducer";
 
 const Dialogs = (props) => {
-    let dialogsArr = props.dialogs
+
+    let dialogs = useSelector((state) => state.dialogsPage.dialogs);
+    let messages = useSelector((state) => state.dialogsPage.messages);
+    let messageText = useSelector((state) => state.dialogsPage.messageText);
+
+    let dispatch = useDispatch()
+
+    let dialogsArr = dialogs
     .map(el => <DialogItem name={el.name} key={el.id} id={el.id} />)
 
-    let messagesArr = props.messages
+    let messagesArr = messages
     .map(el => <Message key={el.id} message={el.message} />)
 
 
     let messageTextChange = (event) => {
-        props.messageTextChange(event.target.value)
+        // props.messageTextChange(event.target.value)
+        dispatch(messageTextChangeActionCreator(event.target.value))
     }
 
     let addMessage = () => {
-        props.addMessage()
+        // props.addMessage()
+        dispatch(addMessageActionCreator())
     }
 
     return (
@@ -30,7 +41,7 @@ const Dialogs = (props) => {
             </div>
 
             <div className={classes.messageInput}>
-                <input className={classes.messageText} size={40} onChange={messageTextChange} cols="30" rows="1" value={props.messageText}></input>
+                <input className={classes.messageText} size={40} onChange={messageTextChange} cols="30" rows="1" value={messageText}></input>
                 <button className={classes.sendMessage} onClick={addMessage}>Отправить</button>
             </div>
         </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,32 +3,37 @@ import React from "react";
 import UserItem from "./UserItem/UserItem";
 import axios from "axios";
 
-let Users = props => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                debugger
-
-                props.setUsers(response.data.items);
-            })
+class Users extends React.Component {
+    getUsers = () => {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                })
     }
 
-    let usersArr = props.users.map(el => <UserItem id={el.id} key={el.id} name={el.name} photo={el.photos.small}
-                                                   followed={el.followed} status={el.status}
-                                                   follow={props.follow} unfollow={props.unfollow}/>)
+    componentDidMount() {
+        this.getUsers();
+    }
 
-    return (<div className={classes.body}>
-        <div className={classes.titleWrapper}>
-            <h1>
-                Users
-            </h1>
-        </div>
-        {usersArr.slice(0, props.showAmount)}
-        <div className={classes.showMoreWrapper}>
-            {usersArr.length > props.showAmount ?
-                <button onClick={props.showMore} className={classes.showMore}>Show more</button> : null}
-        </div>
-    </div>)
+    render() {
+        let usersArr = this.props.users.map(el => <UserItem id={el.id} key={el.id} name={el.name} photo={el.photos.small}
+                                                               followed={el.followed} status={el.status}
+                                                               follow={this.props.follow} unfollow={this.props.unfollow}/>)
+        return (<div>
+            <div className={classes.titleWrapper}>
+                <h1>
+                    Users
+                </h1>
+            </div>
+            {usersArr.slice(0, this.props.showAmount)}
+            <div className={classes.showMoreWrapper}>
+                {usersArr.length > this.props.showAmount ?
+                    <button onClick={this.props.showMore} className={classes.showMore}>Show more</button> : null}
+            </div>
+        </div>)
+    }
+
 }
+
 
 export default Users;
