d654705832acbf715c7d54130860845dac20a364
Maxim Shtin <maksimshtin@gmail.com>
2025-07-12 12:59:18 +0500

    If user is authenticated then his login is shown in the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,38 @@
 import logo from "../../assets/Logo.png"
 import classes from "./Header.module.css"
+import {NavLink} from "react-router-dom";
+import {useEffect} from "react";
+import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {setUserDataActionCreator} from "../../redux/authReducer";
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const isAuth = useSelector(state => state.auth.isAuth)
+    const login = useSelector(state => state.auth.login)
+
+    useEffect(() => {
+        axios.get("https://social-network.samuraijs.com/api/1.0/auth/me", {
+            withCredentials: true,
+        })
+            .then((response) => {
+                if (response.data.resultCode === 0) {
+                    dispatch(setUserDataActionCreator(response.data.data));
+                }
+            })
+    }, []);
+
+
+
     return (
         <header className={classes.header}>
-            <img className={classes.logo} src={logo} alt="Logo" />
+            <img className={classes.logo} src={logo} alt="Logo"/>
+            <div className={classes.login}>
+                {isAuth
+                    ? <p className={classes.loginName}>{login}</p>
+                    : <NavLink className={classes.loginText} to={"/login"}>Login</NavLink>}
+
+            </div>
         </header>
     )
 }
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -5,13 +5,15 @@ import friendsPageReducer from "./friendsPageReducer";
 import profileReducer from "./profileReducer";
 import usersReducer from "./usersReducer";
 import musicReducer from "./musicReducer";
+import authReducer from "./authReducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     friendsPage: friendsPageReducer,
     usersPage: usersReducer,
-    musicPage: musicReducer
+    musicPage: musicReducer,
+    auth: authReducer,
 });
 
 let store = configureStore({reducer: reducers});
