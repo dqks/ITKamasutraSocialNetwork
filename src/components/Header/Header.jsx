40a429bee2b9686fd742aab7d9745463790e5104
Maxim Shtin <maksimshtin@gmail.com>
2025-07-12 13:14:59 +0500

    Adds user image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,14 @@ import {NavLink} from "react-router-dom";
 import {useEffect} from "react";
 import axios from "axios";
 import {useDispatch, useSelector} from "react-redux";
-import {setUserDataActionCreator} from "../../redux/authReducer";
+import {setCurrentUserImageActionCreator, setUserDataActionCreator} from "../../redux/authReducer";
+import avatar from "../../assets/avatar.jpg"
 
 const Header = () => {
     const dispatch = useDispatch();
     const isAuth = useSelector(state => state.auth.isAuth)
     const login = useSelector(state => state.auth.login)
+    let photo = useSelector(state => state.auth.userPhoto)
 
     useEffect(() => {
         axios.get("https://social-network.samuraijs.com/api/1.0/auth/me", {
@@ -18,18 +20,31 @@ const Header = () => {
             .then((response) => {
                 if (response.data.resultCode === 0) {
                     dispatch(setUserDataActionCreator(response.data.data));
+                    axios.get("https://social-network.samuraijs.com/api/1.0/profile/" + response.data.data.id)
+                        .then((response) => {
+                            if (response.data.photos.small) {
+                                dispatch(setCurrentUserImageActionCreator(response.data.photos.small));
+                            } else {
+                                dispatch(setCurrentUserImageActionCreator("default"));
+                            }
+                        })
                 }
             })
     }, []);
 
-
+    if (photo === "default") {
+        photo = avatar
+    }
 
     return (
         <header className={classes.header}>
             <img className={classes.logo} src={logo} alt="Logo"/>
             <div className={classes.login}>
                 {isAuth
-                    ? <p className={classes.loginName}>{login}</p>
+                    ? <div className={classes.userInfoWrapper}>
+                        <img className={classes.photo} src={photo} alt="Photo"/>
+                        <p className={classes.loginName}>{login}</p>
+                    </div>
                     : <NavLink className={classes.loginText} to={"/login"}>Login</NavLink>}
 
             </div>
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,22 +1,28 @@
 const SET_USER_DATA = "SET_USER_DATA";
+const SET_CURRENT_USER_IMAGE = "SET_CURRNET_USER_IMAGE";
 
 let initialState = {
     userId: null,
     email: null,
     login: null,
     isFetching: false,
-    isAuth: false
+    isAuth: false,
+    userPhoto: null,
 };
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_DATA:
             return {...state, ...action.data, isAuth: true};
+        case SET_CURRENT_USER_IMAGE:
+            debugger
+            return {...state, userPhoto: action.userPhoto};
         default:
-            return state
+            return {...state, userPhoto: action.userPhoto};
     }
 }
 
 export const setUserDataActionCreator = data => ({type: SET_USER_DATA, data});
+export const setCurrentUserImageActionCreator = userPhoto => ({type: SET_CURRENT_USER_IMAGE, userPhoto});
 
 export default authReducer;
